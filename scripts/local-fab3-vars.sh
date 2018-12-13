5ca7283be7d91612c59de4ca4041f88e5d26f9c1
Swetha Repakula <srepaku@us.ibm.com>
2018-12-13 21:14:00 +0100

    Fab3 env vars based on  env var

--- scripts/local-fab3-vars.sh
+++ scripts/local-fab3-vars.sh
@@ -1,4 +1,4 @@
-export FABPROXY_CONFIG="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"/../configs/local.yaml
+export FABPROXY_CONFIG=${WORKSHOP}/configs/local.yaml
 export FABPROXY_CHANNEL=mychannel
 export FABPROXY_CCID=evmcc
 export FABPROXY_ORG=Org1
--- scripts/public-fab3-vars.sh
+++ scripts/public-fab3-vars.sh
@@ -2,7 +2,7 @@ USER_ID=$1
 ORG_NUMBER=$2
 CHANNEL=$3
 
-export FABPROXY_CONFIG="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"/../configs/fab3.yaml
+export FABPROXY_CONFIG=$WORKSHOP/configs/public.yaml
 export FABPROXY_CHANNEL=$CHANNEL
 export FABPROXY_CCID=evmcc
 export FABPROXY_ORG=Org${ORG_NUMBER}
